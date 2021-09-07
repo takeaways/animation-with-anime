aaa198387570cbc625f162b8e1e41d0441214cee
lucas.j <lucas.j@hpcnt.com>
2021-09-08 00:32:14 +0900

    slider

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -7,4 +7,19 @@ new fullpage("#fullpage", {
   scrollHorizontally: true,
   navigation: true,
   anchors: ["num0", "num1", "num2", "num3", "num4"],
+  afterLoad: (old_el, new_el) => {
+    if (new_el.index === 1) {
+      sec2();
+    }
+    if (old_el.index === 2) {
+      sec2_reset();
+    }
+  },
 });
+
+function sec2() {
+  console.log("s");
+}
+function sec2_reset() {
+  console.log("r");
+}
