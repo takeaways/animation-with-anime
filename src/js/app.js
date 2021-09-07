e29c8cbfacd7d11f51ca18277e074bd3e9ccf1f6
lucas.j <lucas.j@hpcnt.com>
2021-09-07 22:46:01 +0900

    setting navigation

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import fullpage from "fullpage.js";
 import "fullpage.js/dist/fullpage.min.css";
+import "../css/styles.css";
 
 new fullpage("#fullpage", {
   //options here
   autoScrolling: true,
   scrollHorizontally: true,
+  navigation: true,
 });
