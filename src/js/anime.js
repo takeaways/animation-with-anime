d2150caa1ceeba3ffb122860a156344869625e37
lucas.j <lucas.j@hpcnt.com>
2021-09-08 02:21:00 +0900

    path good

--- src/js/anime.js
+++ src/js/anime.js
@@ -1,57 +1,12 @@
 import anime from "animejs/lib/anime.es.js";
 
-const staggerWrap = document.querySelector("ul");
-const fragment = document.createDocumentFragment();
-const grid = [9, 5];
-const col = grid[0];
-const row = grid[1];
-const allElem = col * row;
-
-for (let i = 0; i < allElem; i++) {
-  const li = document.createElement("li");
-  li.setAttribute("class", "box");
-  fragment.appendChild(li);
-}
-
-staggerWrap.appendChild(fragment);
-
-const tl = anime.timeline({
-  targets: "ul li.box",
-  delay: anime.stagger(200, {
-    grid: [9, 5],
-    from: "center",
-  }),
+const path = anime.path(".svg path");
+anime({
+  targets: ".circle",
+  translateX: path("x"),
+  translateY: path("y"),
+  rotate: path("angle"),
+  easing: "linear",
+  duration: 2000,
   loop: true,
-  direction: "alternate",
-});
-
-tl.add({
-  scale: [
-    {
-      value: 0.1,
-      easing: "easeOutSine",
-      duration: 500,
-    },
-    {
-      value: 1,
-      easing: "easeOutQuad",
-      duration: 1200,
-    },
-  ],
-}).add({
-  translateX: anime.stagger(10, {
-    grid: [9, 5],
-    from: "center",
-    axis: "x",
-  }),
-  translateY: anime.stagger(10, {
-    grid: [9, 5],
-    from: "center",
-    axis: "y",
-  }),
-  rotate: anime.stagger([0, 90], {
-    grid: [9, 5],
-    from: "center",
-    axis: "x",
-  }),
 });
